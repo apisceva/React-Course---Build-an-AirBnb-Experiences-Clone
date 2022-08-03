80dc1f36ee5f151e05c4f34c62d71046afa35501
apisceva <aljona.gromova@gmail.com>
2022-08-03 09:51:07 -0400

    working with Data and App

--- src/App.js
+++ src/App.js
@@ -3,13 +3,25 @@ import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
+import Data from "./Data"
 
 export default function App() {
+    const cards = Data.map(item => {
+        return (
+            <Card 
+                key={item.id}
+                {...item}
+            />
+        )
+    })
+
     return (
         <div>
             <Navbar />
             <Hero /> 
-            <Card />
+           <section className="cards-list">
+                {cards}
+            </section> 
         </div>
     )
 }
