c5704babfbd0b6e063a7036512ea19c9a79e7981
apisceva <aljona.gromova@gmail.com>
2022-08-03 10:14:49 -0400

    final

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,8 +10,14 @@ export default function Card(props) {
     
     return (
         <div className="card">
-            {badgeText && <div className="card--badge">{badgeText}</div>}
-            <img src={require(`../images/${props.coverImg}`)} className="card--image" />
+            {
+            badgeText && 
+            <div className="card--badge">{badgeText}</div>
+            }
+            <img 
+                src={require(`../images/${props.coverImg}`)} 
+                className="card--image" 
+            />
             <div className="card--stats">
                 <img src={require("../images/star.png")} className="card--star" />
                 <span>{props.stats.rating}</span>
